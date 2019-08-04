b0a1871a72a115c2ca05168182039526d67ccdcb
lijunchen <171869092@qq.com>
2019-08-04 18:49:18 +0800

    test authorization

--- laravel/app/Http/Controllers/authorization.php
+++ laravel/app/Http/Controllers/authorization.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request as req;
 use Illuminate\Routing\Controller as BaseController;
 use App\Table\author as auth;
 use MWSClientTest as ts;
@@ -17,15 +18,24 @@ class authorization extends BaseController{
 
     }
 
-    public static function runAuthorization()
+    public static function runAuthorization(req $request)
     {
+        $data = $request->all();
         $client = new MC([
-//            'Marketplace_Id' => 'A1PA6795UKMFR9',
-            'Seller_Id' => 'A1XKW8C3I72FG6',
-            'Access_Key_ID' => 'AKIAJ3WT3MQK7V5I2BIQ',
-            'Secret_Access_Key' => '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',
-            'MWSAuthToken' => 'amzn.mws.a4b4182e-02ba-5bc7-6b26-1ab2dd11f244' // Optional. Only use this key if you are a third party user/developer
+            'Marketplace_Id' => 'A1F83G8C2ARO7P',
+            'Seller_Id' => $data['Seller_Id'],
+            'Access_Key_ID' => $data['Access_Key_ID'],
+            'Secret_Access_Key' => $data['Secret_Access_Key'],
+            'MWSAuthToken' => $data['MWSAuthToken'] // Optional. Only use this key if you are a third party user/developer
         ]);
+
+//        $client = new MC([
+////            'Marketplace_Id' => 'A1PA6795UKMFR9',
+//            'Seller_Id' => 'A1XKW8C3I72FG6',
+//            'Access_Key_ID' => 'AKIAJ3WT3MQK7V5I2BIQ',
+//            'Secret_Access_Key' => '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',
+//            'MWSAuthToken' => 'amzn.mws.a4b4182e-02ba-5bc7-6b26-1ab2dd11f244' // Optional. Only use this key if you are a third party user/developer
+//        ]);
 //        $app = auth::all();
 //        $product = new pro();
 //        $product->sku = 'TESTNOTFORSALE';
